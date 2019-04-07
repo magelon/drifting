4866d9ff2511dbcd4c89853e20f5de06438c9a0b
magelon <alfardproject@gmail.com>
2019-04-07 00:26:25 -0700

    c

---
+++ Assets/scripts/Drifting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drifting : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //transform.Translate(transform.up*0.01f);
+        rb.AddForce(transform.up*0.01f);
+        if (Input.GetMouseButton(0))
+        {
+            rb.AddTorque(0.01f);
+            
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            rb.angularVelocity = 0;
+            
+        }
+        if (Input.GetMouseButton(1))
+        {
+            rb.AddTorque(-0.01f);
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            rb.angularVelocity = 0;
+        }
+    }
+}
