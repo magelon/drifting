84b2c07b1ad2386c343f10d72eee923981ffecce
magelon <alfardproject@gmail.com>
2019-04-10 16:36:38 -0700

    c

--- Assets/scripts/UI/tapToGo.cs
+++ Assets/scripts/UI/tapToGo.cs
@@ -2,78 +2,98 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using AudienceNetwork;
+using GoogleMobileAds.Api;
+using System;
 
 public class tapToGo : MonoBehaviour
 {
     public Rigidbody2D rb;
     public GameObject startText;
-    private InterstitialAd interstitialAd;
-    private bool isLoaded;
 
-    private void Start()
-    {
-        Loadintersitital();
-    }
+    private RewardBasedVideoAd rewardBasedVideo;
 
-    void OnDestroy()
+    public void ShowRewardBasedVideo()
     {
-        // Dispose of interstitial ad when the scene is destroyed
-        if (interstitialAd != null)
+        if (this.rewardBasedVideo.IsLoaded())
         {
-            interstitialAd.Dispose();
-        }
-        Debug.Log("InterstitialAdTest was destroyed!");
-    }
-
-    public void Loadintersitital()
-    {
-        this.interstitialAd = new InterstitialAd("2222185464713314_2222189018046292");
-        this.interstitialAd.Register(this.gameObject);
-
-        // Set delegates to get notified on changes or when the user interacts with the ad.
-        this.interstitialAd.InterstitialAdDidLoad = (delegate () {
-            Debug.Log("Interstitial ad loaded.");
-            this.isLoaded = true;
-        });
-        interstitialAd.InterstitialAdDidFailWithError = (delegate (string error) {
-            Debug.Log("Interstitial ad failed to load with error: " + error);
-        });
-        interstitialAd.InterstitialAdWillLogImpression = (delegate () {
-            Debug.Log("Interstitial ad logged impression.");
-        });
-        interstitialAd.InterstitialAdDidClick = (delegate () {
-            Debug.Log("Interstitial ad clicked.");
-        });
-
-        this.interstitialAd.interstitialAdDidClose = (delegate () {
-            Debug.Log("Interstitial ad did close.");
-
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-
-            if (this.interstitialAd != null)
-            {
-                this.interstitialAd.Dispose();
-            }
-        });
-
-        // Initiate the request to load the ad.
-        this.interstitialAd.LoadAd();
-    }
-
-    public void ShowInterstitial()
-    {
-        if (this.isLoaded)
-        {
-            this.interstitialAd.Show();
-            this.isLoaded = false;
-
+            this.rewardBasedVideo.Show();
         }
         else
         {
-            Debug.Log("Interstitial Ad not loaded!");
+            MonoBehaviour.print("Reward based video ad is not ready yet");
         }
     }
+
+    private void Start()
+    {
+        #if UNITY_ANDROID
+        string appId = "ca-app-pub-3308520213502941~1034075722";
+        #elif UNITY_IPHONE
+            string appId = "ca-app-pub-3940256099942544~1458002511";
+#else
+            string appId = "unexpected_platform";
+#endif
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);
+
+        // Get singleton reward based video ad reference.
+        this.rewardBasedVideo = RewardBasedVideoAd.Instance;
+
+        // Called when an ad request has successfully loaded.
+        rewardBasedVideo.OnAdLoaded += HandleRewardBasedVideoLoaded;
+        // Called when an ad request failed to load.
+        rewardBasedVideo.OnAdFailedToLoad += HandleRewardBasedVideoFailedToLoad;
+        // Called when an ad is shown.
+        rewardBasedVideo.OnAdOpening += HandleRewardBasedVideoOpened;
+        // Called when the ad starts to play.
+        rewardBasedVideo.OnAdStarted += HandleRewardBasedVideoStarted;
+        // Called when the user should be rewarded for watching a video.
+        rewardBasedVideo.OnAdRewarded += HandleRewardBasedVideoRewarded;
+        // Called when the ad is closed.
+        rewardBasedVideo.OnAdClosed += HandleRewardBasedVideoClosed;
+        // Called when the ad click caused the user to leave the application.
+        rewardBasedVideo.OnAdLeavingApplication += HandleRewardBasedVideoLeftApplication;
+
+        this.RequestRewardBasedVideo();
+
+
+    }
+
+    private void OnDisable()
+    {
+        // Called when an ad request has successfully loaded.
+        rewardBasedVideo.OnAdLoaded -= HandleRewardBasedVideoLoaded;
+        // Called when an ad request failed to load.
+        rewardBasedVideo.OnAdFailedToLoad -= HandleRewardBasedVideoFailedToLoad;
+        // Called when an ad is shown.
+        rewardBasedVideo.OnAdOpening -= HandleRewardBasedVideoOpened;
+        // Called when the ad starts to play.
+        rewardBasedVideo.OnAdStarted -= HandleRewardBasedVideoStarted;
+        // Called when the user should be rewarded for watching a video.
+        rewardBasedVideo.OnAdRewarded -= HandleRewardBasedVideoRewarded;
+        // Called when the ad is closed.
+        rewardBasedVideo.OnAdClosed -= HandleRewardBasedVideoClosed;
+        // Called when the ad click caused the user to leave the application.
+        rewardBasedVideo.OnAdLeavingApplication -= HandleRewardBasedVideoLeftApplication;
+    }
+
+    private void RequestRewardBasedVideo()
+    {
+
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-3308520213502941/3746146778";
+        //demo id ca-app-pub-3940256099942544/5224354917;
+#elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-3940256099942544/1712485313";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        AdRequest request = new AdRequest.Builder().Build();
+
+        this.rewardBasedVideo.LoadAd(request, adUnitId);
+    }
+
 
     public void TapToGo()
     {
@@ -83,7 +103,52 @@ public class tapToGo : MonoBehaviour
     }
     public void Restart()
     {
-        ShowInterstitial();
+        ShowRewardBasedVideo();
         //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+    #region RewardBasedVideo callback handlers
+
+    public void HandleRewardBasedVideoLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoLoaded event received");
+    }
+
+    public void HandleRewardBasedVideoFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print(
+            "HandleRewardBasedVideoFailedToLoad event received with message: " + args.Message);
+    }
+
+    public void HandleRewardBasedVideoOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoOpened event received");
+    }
+
+    public void HandleRewardBasedVideoStarted(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoStarted event received");
+    }
+
+    public void HandleRewardBasedVideoClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoClosed event received");
+    }
+    //add reward to player
+    public void HandleRewardBasedVideoRewarded(object sender, Reward args)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        MonoBehaviour.print(
+            "HandleRewardBasedVideoRewarded event received for " );
+    }
+
+    public void HandleRewardBasedVideoLeftApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoLeftApplication event received");
+    }
+
+    #endregion
+
+
+
 }
